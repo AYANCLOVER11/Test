45179700ad2b619dd25c94440a8d8c9e06130612
AYANCLOVER11 <ianclavero6422@gmail.com>
2024-09-23 00:08:23 +0800

    Update Test.cpp
    test 2

--- Test.cpp
+++ Test.cpp
@@ -7,6 +7,8 @@ int main() {
 	cout << "Hello Kalibutan " << endl;
 cout << "Second Commit in this code " << endl ;
 
+	//try daw pag edit or write in this code kung ma kaya daw example like cout << trial etc.
+	
 	return 0;
 	
 }
